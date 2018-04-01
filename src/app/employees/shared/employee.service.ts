ed0a61bc39f7996c1a174ea0407e05bb94b79194
Tabuzo, Elmo Jr V <elmo.jr.v.tabuzo@accenture.com>
2018-04-01 13:37:23 +0800

    Angular WEB API Restful

---
+++ src/app/employees/shared/employee.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions, RequestMethod } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+import { Employee } from './employee.model';
+
+@Injectable()
+export class EmployeeService {
+
+  selectedEmployee: Employee;
+  constructor(private http : Http) { }
+
+  postEmployee(emp : Employee) {
+    var body = JSON.stringify(emp);
+    var headerOptions = new Headers({'Content-Type':'application/json'});
+    var requestOptions = 
+      new RequestOptions({method: RequestMethod.Post, headers : headerOptions});
+
+    return this
+      .http
+      .post('http://localhost:57842/api/Employees', body, requestOptions)
+      .map(x => x.json());
+  }
+
+}
